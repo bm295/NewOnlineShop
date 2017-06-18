f0562d086a4d339ed2d949436d12d8a5f0068034
bm295 <baominh.nguyen.295@gmail.com>
2017-06-18 15:57:03 +0800

    add entity framework

---
+++ Model/DAO/UserService.cs
@@ -0,0 +1,30 @@
+﻿using Model.EntityFramework;
+using System.Linq;
+
+namespace Model.DAO
+{
+    public class UserService
+    {
+        OnlineShopDbContext onlineShopDbContext = null;
+
+        public UserService()
+        {
+            onlineShopDbContext = new OnlineShopDbContext();
+        }
+
+        public long Insert(User user)
+        {
+            onlineShopDbContext.Users.Add(user);
+            onlineShopDbContext.SaveChanges();
+            return user.Id;
+        }
+
+        public bool Login(string username, string password)
+        {
+            var result = onlineShopDbContext.Users.Count(x => x.Username == username && x.Password == password);
+            if (result > 0)
+                return true;
+            return false;
+        }
+    }
+}
